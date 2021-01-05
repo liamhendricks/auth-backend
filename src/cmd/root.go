40d165e81f434c9df00c99accf9376703f5d0e9b
Liam Hendricks <liamh.blogg@gmail.com>
2021-01-05 15:48:36 -0700

    logging...

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/68696c6c/goat"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -14,4 +16,13 @@ var RootCommand = &cobra.Command{
 func init() {
 	cobra.OnInitialize(goat.Init)
 	viper.AutomaticEnv()
+
+	v := os.Getenv("HTTP_PORT")
+	println(v)
+	v = os.Getenv("DB_DATABASE")
+	println(v)
+	v = os.Getenv("DB_USERNAME")
+	println(v)
+	v = os.Getenv("ENV")
+	println(v)
 }
